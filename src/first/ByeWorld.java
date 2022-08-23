a0d739546e97e4ca4b6d3553fe92d1946dddb892
your name <your email>
2022-08-23 15:24:32 -0400

    added ByeWorld.java

---
+++ src/first/ByeWorld.java
@@ -0,0 +1,8 @@
+package first;
+
+public class ByeWorld {
+    public static void main(String[] args) {
+        System.out.println("Maybe 5 wees of java left");
+        System.out.println("1.5 weeks of git");
+    }
+}
